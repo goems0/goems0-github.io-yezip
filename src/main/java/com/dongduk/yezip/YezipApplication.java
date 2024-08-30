0cf2e616b3c85aa83f453908761adeedae1c74b4
goems0 <80386156+goems0@users.noreply.github.com>
2024-08-30 17:33:20 +0900

    Add files via upload

---
+++ src/main/java/com/dongduk/yezip/YezipApplication.java
@@ -0,0 +1,13 @@
+package com.dongduk.yezip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YezipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YezipApplication.class, args);
+	}
+
+}
