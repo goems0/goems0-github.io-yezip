2381753a9c26b95a85d8359e5442b027f52d48fa
goems0 <goems0@naver.com>
2024-08-30 17:16:01 +0900

    init spring

---
+++ yezip/src/test/java/com/dongduk/yezip/YezipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongduk.yezip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YezipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
