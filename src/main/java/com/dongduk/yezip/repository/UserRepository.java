d9c16ea49e3e7d596e61adefd77dc3dceb2f2543
dohyun0123 <cherry_0123@naver.com>
2024-09-30 17:16:11 +0900

    ..n..

--- src/main/java/com/dongduk/yezip/repository/UserRepository.java
+++ src/main/java/com/dongduk/yezip/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.dongduk.yezip.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.dongduk.yezip.domain.Author;
+import com.dongduk.yezip.domain.Profile;
 import com.dongduk.yezip.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
@@ -19,14 +21,16 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     int findByUserIdAndPhoneAndEmail(String name, String phone, String email);
     
     // 비밀번호 재설정
-    
+    int updatePw(String newpw, String checkPw);
     // 로그아웃
     
     // 회원정보 수정
+    int updateUser(User user);
     
     // 회원탈퇴
+    int deleteUser(User user);
     
     // 프로필 설정
-    
+    int updateProfile(int uid, Profile profile, Author author);
     //
 }
