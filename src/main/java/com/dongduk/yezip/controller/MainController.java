d10fb06035307ca5c2d362f1c48cb9c1d7e9381d
goems0 <goems0@naver.com>
2024-10-13 16:08:59 +0900

    create controller

--- src/main/java/com/dongduk/yezip/controller/MainController.java
+++ src/main/java/com/dongduk/yezip/controller/MainController.java
@@ -4,9 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-    @GetMapping({"/", "/home"})
-    public String home() {
-        return "home"; 
+public class MainController {
+    @GetMapping({"/", "/main"})
+    public String main() {
+        return "main"; 
+    }
+    @GetMapping("/whole-items")
+    public String showWholeItems() {
+        return "whole-items"; 
     }
 }
